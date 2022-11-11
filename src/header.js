8beb7aeed9dd0aa50cc5cf2df77dee3ab271d304
mario.murd@gmail.com <61310052+Hungwy@users.noreply.github.com>
2022-11-11 16:12:45 +0200

    first

---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Header() {
+    return(
+        <nav className="">
+            <div className="">
+                <h3>Registration</h3>
+            </div>
+        </nav>
+    )
+}
+export default Header;
